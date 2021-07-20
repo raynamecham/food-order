be7f21d205b17a0864ae20a078fcd038003481a5
raynamecham <rayna.mecham@gmail.com>
2021-07-20 17:46:02 -0600

    setting up index.php

---
+++ admin/index.php
@@ -0,0 +1,32 @@
+<html>
+
+<head>
+    <title>Food Order Website - Home Page</title>
+
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+    <!-- Menu Section Starts -->
+    <div class="menu">
+        <div class="wrapper">Menu Goes Here</div>
+    </div>
+    <!-- Menu Section Ends -->
+
+    <!-- Main Content Starts -->
+    <div class="main-content">
+        <div class="wrapper">Main Content Goes Here</div>
+    </div>
+    <!-- Main Content Ends -->
+
+    <!-- Footer Section Starts -->
+    <div class="footer">
+        <div class="wrapper">
+            <p>2021 All Rights Reserved, Some Restaurant. Developed by - <a href="https://www.linkedin.com/in/rayna-mecham/" target="_blank">Rayna Mecham</a></p>
+        </div>
+    </div>
+    <!-- Footer Section Ends -->
+</body>
+
+
+</html>
